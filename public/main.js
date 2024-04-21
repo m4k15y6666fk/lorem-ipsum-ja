ca1dfa496c37998bad150b1b97877902efaac38b
hiiro <m4k15y6666fk@outlook.jp>
2024-04-22 00:55:58 +0900

    Converted the entire project to astro template.

---
+++ public/main.js
@@ -0,0 +1,43 @@
+
+window.addEventListener('DOMContentLoaded', async _ => {
+    console.log("loaded");
+
+    const response = await fetch("source/a.txt");
+    localStorage.setItem('original text', await response.text());
+
+    const textarea = document.querySelector('#text-area');
+    const text = document.querySelector('#select-text');
+    //document.querySelector('#select-text').addEventListener('change', (e) => {
+    //    textarea.style.borderColor = "none";
+    //    const response = await fetch(e.target.value);
+    //    localStorage.setItem('original text', await response.text());
+    //});
+    document.querySelector('#select-newline').addEventListener('change', (e) => {
+        textarea.style.borderColor = "unset";
+        if (e.target.value == "true") {
+            textarea.textContent = localStorage.getItem('original text');
+        } else {
+            textarea.textContent = localStorage.getItem('original text').replace(/\n/g, '');
+        }
+    });
+
+    document.querySelector('#copy-button').addEventListener('click', async _ => {
+        navigator.clipboard.writeText(textarea.textContent)
+        .then(_ => {
+            textarea.style.borderColor = "lightgreen";
+        })
+        .catch(_ => {
+            textarea.style.borderColor = "red";
+        });
+    });
+    document.querySelector('#download-button').addEventListener('click', _ => {
+        const file = new File([textarea.textContent], text.value, {
+            type: "text/plain",
+        });
+
+        const _a = document.createElement('a');
+        _a.href = URL.createObjectURL(file);
+        _a.setAttribute("download", text.value);
+        _a.click();
+    });
+});
